98a821914bf9fa45c57999ec2d595b3508c62595
andretokumoto <andretokumoto@gmail.com>
2024-08-20 16:25:21 -0300

    url e começe de views

--- FormataiBackEnd/FormataiBE_API/urls.py
+++ FormataiBackEnd/FormataiBE_API/urls.py
@@ -1,2 +1,8 @@
 from django.urls import path
 from . import views
+
+
+#rotas
+urlpatterns = [
+    path('get-pdf/',views.getpdf),#rota para gerar pdf
+]
--- FormataiBackEnd/FormataiBE_API/views.py
+++ FormataiBackEnd/FormataiBE_API/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+import datetime
+from rest_framework.response import Response
+from rest_framework import status,viewsets
+from rest_framework.decorators import api_view, throttle_classes ,permission_classes
 
-# Create your views here.
+
+@api_view(['GET','POST'])
+def getpdf(request):
+    
+    if request.method == 'GET':
+     ...
+
