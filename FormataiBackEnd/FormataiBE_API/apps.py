8c5d17af29eae5dd71749568f2fa56ccd676845d
andretokumoto <andretokumoto@gmail.com>
2024-08-20 16:12:42 -0300

    instalação DRF

--- FormataiBackEnd/FormataiBE_API/apps.py
+++ FormataiBackEnd/FormataiBE_API/apps.py
@@ -3,4 +3,4 @@ from django.apps import AppConfig
 
 class FormataibeConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'FormataiBE'
+    name = 'FormataiBE_API'
