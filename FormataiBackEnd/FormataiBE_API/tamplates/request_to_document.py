782aa86561e7e9fc5cd65b2aeb9a14a08a59427e
andretokumoto <andretokumoto@gmail.com>
2024-08-21 16:50:35 -0300

    post para geração de arquivo latex e função de geração do arquivo de oficio a partir dos dados - falta expandir para os outros arquivos

--- FormataiBackEnd/FormataiBE_API/tamplates/request_to_document.py
+++ FormataiBackEnd/FormataiBE_API/tamplates/request_to_document.py
@@ -1,25 +1,47 @@
+def open_template(template_path):
+    #le os dados do template e salva na variavel template
+    with open(template_path, 'r') as template_file:
+      template = template_file.read()  
+    return template
+
+def salva_arquivo(output_path,doc_gerado):
+    with open(output_path, 'w') as output_file:#abre o arquivo de entrada para edição
+        template = output_file.write(doc_gerado)  
+
 def to_pdf(dados):
     ...
     
 def to_tex(dados):
     
-    document_type = dados.get('document_type')
+    document_type = dados.get('document_type')#verifica o tipo de documento requerido
+    output_path = 'FormataiBE_API/tampletes/arquivo_gerado.tex'   #arquivo gerado
     
     #verifica o tipo de documento requerido
+    
+    '''----- OFICIO ------------------------'''
+    doc_gerado = None
+    
     if document_type == 'oficio':
-        template_path = 'FormataiBE_API//tampletes/oficio.tex'  #template
-        output_path = 'FormataiBE_API/tampletes/arquivo_gerado.tex'   #arquivo gerado
+        template_path = 'FormataiBE_API//tampletes/oficio.tex'  #template oficio
+        template = open_template(template_path)#le os dados do template
+        
+        #substitui os dados do template pelo do requerimento
+        doc_gerado = template.replace('__OFICIO_NUMERO__', dados.get('oficio_numero', ''))
+        doc_gerado = template.replace('__DESTINATARIO__', dados.get('destinatario', ''))
+        doc_gerado = template.replace('__ENDERECO_DESTINATARIO__', dados.get('endereco_destinatario', ''))
+        doc_gerado = template.replace('__CIDADE_ESTADO__', dados.get('cidadeEstado_destinatario', ''))
+        doc_gerado = template.replace('__CEP__', dados.get('cep_destinatario', ''))
+        doc_gerado = template.replace('__TEXTO_SOLICITACAO__', dados.get('corpo_mensagem', ''))
+        doc_gerado = template.replace('__NOME_REMETENTE__', dados.get('nome_remetente', ''))
+        doc_gerado = template.replace('__CARGO_REMETENTE__', dados.get('cargo_remetente', ''))
+        doc_gerado = template.replace('__ORGANIZACAO_REMETENTE__', dados.get('organizacao_remetente', ''))
+        
+        salva_arquivo(output_path,doc_gerado)
 
-    with open(template_path, 'r') as template_file:#le os dados do template e salva na variavel template
-        template = template_file.read()    
-    
-    #substitui os dados do template pelo do requerimento
-    oficio = template.replace('__OFICIO_NUMERO__', dados.get('oficio_numero', ''))   
-    
-    '''
-    template_path = 'FormataiBE_API/template.tex'  #template
-    output_path = 'FormataiBE_API/resultado.tex'   #arquivo gerado
 
-    with open(template_path, 'r') as template_file:
-        template = template_file.read()
-        '''
+    return output_path
+'''------------------------------------------------------------------------------------------------------------------------------------------------'''
+   
+
+
+    
--- FormataiBackEnd/FormataiBE_API/views.py
+++ FormataiBackEnd/FormataiBE_API/views.py
@@ -5,6 +5,8 @@ from rest_framework.response import Response
 from rest_framework import status,viewsets
 from rest_framework.decorators import api_view, throttle_classes ,permission_classes
 from .tamplates.request_to_document import to_pdf, to_tex
+from django.http import FileResponse
+import os
 
 
 @api_view(['GET','POST'])
@@ -12,4 +14,15 @@ def getpdf(request):
     
     if request.method == 'GET':
      ...
-
+     
+@api_view(['GET','POST'])
+def getLatex(request):
+    
+    if request.method == 'POST':
+        
+        dados = request.data
+        tex_gerado = to_tex(dados)
+        
+        arquivo = FileResponse(tex_gerado, content_type='application/x-tex')
+        arquivo['Content-Disposition'] = f'attachment; filename="{os.path.basename(tex_gerado)}"'
+        return arquivo
