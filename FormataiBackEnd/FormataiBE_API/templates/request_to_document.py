9f1a8375c8d7536780f8e3865f043b0ba9e93be0
andretokumoto <andretokumoto@gmail.com>
2024-08-21 21:12:10 -0300

    latex memorando

--- FormataiBackEnd/FormataiBE_API/templates/request_to_document.py
+++ FormataiBackEnd/FormataiBE_API/templates/request_to_document.py
@@ -1,20 +1,3 @@
-import os
-'''
-dados = {
-  "document_type": "oficio",
-  "oficio_numero": "12345",
-  "destinatario": "João da Silva",
-  "endereco_destinatario": "Rua Exemplo, 123",
-  "cidadeEstado_destinatario": "São Paulo, SP",
-  "cep_destinatario": "01000-000",
-  "assunto": "Solicitação de Informação",
-  "corpo_mensagem": "Venho por meio deste solicitar informações sobre o processo XYZ.",
-  "nome_remetente": "Maria de Souza",
-  "cargo_remetente": "Diretora",
-  "organizacao_remetente": "Empresa ABC"
-}
-'''
-
 def open_template(template_path):
     with open(template_path, 'r') as template_file:
         template = template_file.read()  
@@ -28,17 +11,13 @@ def to_pdf(dados):
     ...
 
 def to_tex(dados):
-    
-
-    #print('estamos em :',os.getcwd())
-
 
     document_type = dados.get('document_type')
     output_path = 'FormataiBE_API/templates/arquivo_gerado.tex'
     
     if document_type == 'oficio':
+
         template_path = 'FormataiBE_API/templates/oficio.tex'
-        #template_path = 'oficio.tex'
         template = open_template(template_path)
         
         doc_gerado = template.replace('__OFICIO_NUMERO__', dados.get('oficio_numero', ''))
@@ -47,6 +26,20 @@ def to_tex(dados):
         doc_gerado = doc_gerado.replace('__CIDADE_ESTADO__', dados.get('cidadeEstado_destinatario', ''))
         doc_gerado = doc_gerado.replace('__CEP__', dados.get('cep_destinatario', ''))
         doc_gerado = doc_gerado.replace('__TEXTO_SOLICITACAO__', dados.get('corpo_mensagem', ''))
+        doc_gerado = doc_gerado.replace('__NOME_REMETENTE__', dados.get('nome_remetente', ''))
+        doc_gerado = doc_gerado.replace('__CARGO_REMETENTE__', dados.get('cargo_remetente', ''))
+        doc_gerado = doc_gerado.replace('__ORGANIZACAO_REMETENTE__', dados.get('organizacao_remetente', ''))
+        
+        salva_arquivo(output_path, doc_gerado)
+
+    elif document_type == 'memorando':
+
+        template_path = 'FormataiBE_API/templates/memorando.tex'
+        template = open_template(template_path)
+        
+        doc_gerado = template.replace('__DESTINATARIO__', dados.get('destinatario', ''))
+        doc_gerado = doc_gerado.replace('__CORPO__', dados.get('corpo_mensagem', ''))
+        doc_gerado = doc_gerado.replace('__ASSUNTO__', dados.get('assunto', ''))
         doc_gerado = doc_gerado.replace('__NOME_REMETENTE__', dados.get('nome_remetente', ''))
         doc_gerado = doc_gerado.replace('__CARGO_REMETENTE__', dados.get('cargo_remetente', ''))
         doc_gerado = doc_gerado.replace('__ORGANIZACAO_REMETENTE__', dados.get('organizacao_remetente', ''))
