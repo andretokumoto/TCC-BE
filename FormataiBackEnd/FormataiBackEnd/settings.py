51074e0fa0f4de84f0091969b9e452b0e20e71ab
andretokumoto <andretokumoto@gmail.com>
2025-04-22 14:50:07 -0300

    sem cors

--- FormataiBackEnd/FormataiBackEnd/settings.py
+++ FormataiBackEnd/FormataiBackEnd/settings.py
@@ -38,7 +38,6 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'rest_framework',
-    'corsheaders',
     'FormataiBE_API',
 ]
 
@@ -50,7 +49,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
-    'corsheaders.middleware.CorsMiddleware',
+   
 ]
 
 ROOT_URLCONF = 'FormataiBackEnd.urls'
